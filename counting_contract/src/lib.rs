86984599ad4db003a739ef35428636c6e4a6e4d2
Loredana Cirstea <loredana.cirstea@gmail.com>
2022-10-06 12:39:46 +0300

    CW basics - Chapter 4

--- counting_contract/src/lib.rs
+++ counting_contract/src/lib.rs
@@ -36,3 +36,41 @@ pub fn execute(
 ) -> StdResult<Response> {
     Ok(Response::new())
 }
+
+#[cfg(test)]
+mod test {
+    use crate::msg::{QueryMsg, ValueResp};
+
+    use super::*;
+
+    use cosmwasm_std::{Empty, Addr};
+    use cw_multi_test::{App, Contract, ContractWrapper, Executor};
+
+    fn counting_contract() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(
+            execute,
+            instantiate,
+            query,
+        );
+        Box::new(contract)
+    }
+
+    #[test]
+    fn query_value() {
+        let mut app = App::default();
+        let contract_id = app.store_code(counting_contract());
+        let contract_addr = app.instantiate_contract(
+            contract_id, 
+            Addr::unchecked("sender"),
+            &QueryMsg::Value{value: 4}, 
+            &[], 
+            "Counting", 
+            None,
+        ).unwrap();
+        let resp: ValueResp = app
+            .wrap()
+            .query_wasm_smart(contract_addr, &QueryMsg::Value{value: 4})
+            .unwrap();
+        assert_eq!(resp, ValueResp { value: 5});
+    }
+}
